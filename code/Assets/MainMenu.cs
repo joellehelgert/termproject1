879a4665494300dac3a0e382b5f1c5dd27831e83
Kathi <kathi.resch28@gmail.com>
2019-12-16 18:02:27 +0100

    Overview Menu Main

--- code/Assets/MainMenu.cs
+++ code/Assets/MainMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void CardInformations()
+    public void AgeMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void GoBack()
